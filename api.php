9d3acc35f7ac66b5431f4ca87d61c36bfd2492f4
“Faisolp” <Faisolp@gmail.com>
2018-01-03 00:32:56 +0700

    My first commit

---
+++ api.php
@@ -0,0 +1,76 @@
+<?php
+
+$strAccessToken = "Kyk28/5+VaI8PM/TXF3ZEsf+Nbqw3baIlKZMhmPKmXZaqptqc92e6GrVuLuo1pXlrV8QCzVd2R9lkFKoLsPMdwGTK9/B786h7ws5553DIMg5OgE0W/O49kjzyrN+KY8RPLroGyPy74xR2mzlxf3XqAdB04t89/1O/w1cDnyilFU=";
+
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$_msg = $arrJson['events'][0]['message']['text'];
+
+
+$api_key="GqZCa_5DEsD9s5WqzgDDgLzScENyt-96";
+$url = 'https://api.mlab.com/api/1/databases/frdrlinebot/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/frdrlinebot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$data = json_decode($json);
+$isData=sizeof($data);
+
+if (strpos($_msg, 'สอนBOT’) !== false) {
+  if (strpos($_msg, 'สอนเBOT’) !== false) {
+    $x_tra = str_replace("สอนเป็ด","", $_msg);
+    $pieces = explode("|", $x_tra);
+    $_question=str_replace("[","",$pieces[0]);
+    $_answer=str_replace("]","",$pieces[1]);
+    //Post New Data
+    $newData = json_encode(
+      array(
+        'question' => $_question,
+        'answer'=> $_answer
+      )
+    );
+    $opts = array(
+      'http' => array(
+          'method' => "POST",
+          'header' => "Content-type: application/json",
+          'content' => $newData
+       )
+    );
+    $context = stream_context_create($opts);
+    $returnValue = file_get_contents($url,false,$context);
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนBOT’;
+  }
+}else{
+  if($isData >0){
+   foreach($data as $rec){
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = $rec->answer;
+   }
+  }else{
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = ' คุณสามารถ Add Q&A เพียงพิมพ์: สอนเป็ด[คำถาม|คำตอบ]';
+  }
+}
+
+
+$channel = curl_init();
+curl_setopt($channel, CURLOPT_URL,$strUrl);
+curl_setopt($channel, CURLOPT_HEADER, false);
+curl_setopt($channel, CURLOPT_POST, true);
+curl_setopt($channel, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($channel, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($channel, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($channel);
+curl_close ($channel);
+?>
